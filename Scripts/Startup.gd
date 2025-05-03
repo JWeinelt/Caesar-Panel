c576819367b037f8f47aaf30fe1c0fc6b6b6b279
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-03 19:02:06 +0200

    Added export presets

--- Scripts/Startup.gd
+++ Scripts/Startup.gd
@@ -8,23 +8,27 @@ func _ready():
 	$Username/UEnter.text = GE.cs_settings.defaults.username
 
 
+func login():
+	var username = $Username/UEnter.text
+	var password = $Password/PEnter.text
+	if username.empty() or password.empty():
+		$Error.show()
+		$Error.text = "Please provide a username and a password."
+		return
+	
+
+
 func _on_Exit_pressed():
 	get_tree().quit()
 
 
 func _on_Login_pressed():
-	if $Username/UEnter.text.empty() or $Password/PEnter.text.empty():
-		$Error.show()
-		$Error.text = "Please provide a username and a password."
+	login()
 
 
 func _on_PEnter_text_entered(new_text):
-	if $Username/UEnter.text.empty() or new_text.empty():
-		$Error.show()
-		$Error.text = "Please provide a username and a password."
+	login()
 
 
 func _on_UEnter_text_entered(new_text):
-	if $Password/PEnter.text.empty() or new_text.empty():
-		$Error.show()
-		$Error.text = "Please provide a username and a password."
+	login()
